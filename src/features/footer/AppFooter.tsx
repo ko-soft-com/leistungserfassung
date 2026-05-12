afab067fa8931950ec83d07ef18e4c3a26a202e3
Tobias Koppe <koppetobias@googlemail.com>
2026-05-12 21:53:44 +0200

    refactor(footer): remove redundant __APP_VERSION__ declaration from AppFooter [LEIS-42]

--- src/features/footer/AppFooter.tsx
+++ src/features/footer/AppFooter.tsx
@@ -2,8 +2,6 @@ import changelogRaw from '../../../CHANGELOG.md?raw'
 import ChangelogDialog from './ChangelogDialog'
 import styles from './AppFooter.module.css'
 
-declare const __APP_VERSION__: string
-
 export default function AppFooter() {
   return (
     <footer className={styles.footer} role="contentinfo">
